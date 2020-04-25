66addf4c4da9df0dc4d87680bde427fbca627a1f
Cody Alvarez <jedi.alvarez805@gmail.com>
2020-04-25 14:03:53 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,6 +7,7 @@ def batch_badge_creator(names)
 end
 
 def assign_rooms(attendees)
+<<<<<<< HEAD
   attendees.each_with_index.map { |name, index| "Hello, #{name}! You'll be assigned to room #{index + 1}!" }
 
 end
@@ -14,4 +15,10 @@ end
 def printer(attendees)
 attendees.each_with_index.map { |name| puts badge_maker(name) }
 assign_rooms(attendees).each { |room_names| puts room_names }
+=======
+  attendees.each_with_index { |name, index| "Hello #{name}! You'll be assigned to room #{index + 1}." }
+end
+
+def printer
+>>>>>>> 25545ffda18de1e329355906e8878288c6f05b64
 end
